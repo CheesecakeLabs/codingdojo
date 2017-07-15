c375902da86ef57a1d59e64e36e70eeea85956f4
Ramon Diogo <ramondiogo10.1@gmail.com>
2017-07-15 17:01:26 -0300

    Add play evaluation function

--- challenges/2017-07-kata-poker-dojo/lib/deck.ex
+++ challenges/2017-07-kata-poker-dojo/lib/deck.ex
@@ -2,15 +2,55 @@ defmodule PokerDojo.Deck do
   @ranks Enum.to_list(2..14)
   @suits [:spades, :clubs, :hearts, :diamonds]
 
+
   defmodule Card do
     defstruct [:rank, :suit]
   end
 
-  def new do
+
+  def new_deck do
     for rank <- @ranks, suit <- @suits do
       %Card{rank: rank, suit: suit}
     end
   end
 
-  def test(%Card{rank: 1}), do: 5
+
+  def evaluate(cards) when is_list(cards) do
+    cards
+    |> Enum.map(&to_tuple/1)
+    |> Enum.sort
+    |> eval
+  end
+
+
+  defp to_tuple(
+    %Card{rank: rank, suit: suit}
+  ), do: {rank, suit}
+
+
+  def eval(
+    [{10, s}, {11, s}, {12, s}, {13, s}, {14, s}]
+  ), do: 10 # royal straight flush
+
+  def eval(
+    [{a, s}, {_, s}, {_, s}, {_, s}, {e, s}]
+  ) when e - a == 4, do: 9 # straight flush
+  def eval(
+    [{2, s}, {3, s}, {4, s}, {5, s}, {14, s}]
+  ), do: 9 # straight flush
+
+  def eval(
+    [{a, _}, {a, _}, {a, _}, {a, _}, {_, _}]
+  ), do: 8 # four of a kind
+  def eval(
+    [{_, _}, {a, _}, {a, _}, {a, _}, {a, _}]
+  ), do: 8 # four of a kind
+
+
+  def eval(
+    [{a, _}, {a, _}, {a, _}, {b, _}, {b, _}]
+  ), do: 7 # full house
+  def eval(
+    [{a, _}, {a, _}, {b, _}, {b, _}, {b, _}]
+  ), do: 7 # full house
 end
